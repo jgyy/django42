b03b5d431582768db4998302fecd11703f78efd3
Jeffrey Goh <jegoh@c1r1s3.42singapore.sg>
2024-05-05 20:57:04 +0800

    actual finish admin

--- my_video_rental/videos/admin.py
+++ my_video_rental/videos/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from . import models
 
+class MovieAdmin(admin.ModelAdmin):
+    field = ['release_year', 'title', 'length']
+    search_fields = ['title', 'length']
+    list_filter = ['release_year', 'length', 'title']
+    list_display = ['title', 'release_year', 'length']
+    list_editable = ['length']
+
 admin.site.register(models.Customer)
 admin.site.register(models.Movie)
